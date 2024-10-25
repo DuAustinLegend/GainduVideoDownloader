c0d89c358b29fbe66f5776c9972051d11d7e6dc2
Austin Kalisik <austinkalisik58@gmail.com>
2024-10-25 01:52:36 +0000

    Initiall Commit of Test Project

--- fask_app/app.py
+++ fask_app/app.py
@@ -1,33 +1,38 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os
 
 app = Flask(__name__)
-
-# Import functionalities from the cloned repositories
-# For instance, from video-downloader, vdownloader, SocialDownloader, StreamingCommunity
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/instructions')
+def instructions():
+    return render_template('instructions.html')
 
 @app.route('/download', methods=['POST'])
 def download():
     video_url = request.form['url']
     platform = request.form['platform']
     
-    # Depending on platform, invoke the appropriate downloader
+    # Placeholder for download logic based on platform
     if platform == 'YouTube':
-        # Call YouTube download logic from one of the cloned repos
+        # Implement YouTube download logic
         pass
     elif platform == 'Facebook':
-        # Call Facebook download logic
+        # Implement Facebook download logic
         pass
     elif platform == 'Instagram':
-        # Call Instagram download logic
+        # Implement Instagram download logic
         pass
     elif platform == 'Streaming':
-        # Call streaming platform download logic
+        # Implement Streaming download logic
         pass
+
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
